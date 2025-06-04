95ed5f29adbf22d32f221d0596008e3de00fbd25
Yoriyoi07 <villamingb@students.nu-moa.edu.ph>
2025-06-04 19:22:35 +0800

    yeah

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL);
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true 
 });
 
