341f4eba4e081379f88d6c702227b2692c3fb1a0
Yoriyoi07 <villamingb@students.nu-moa.edu.ph>
2025-06-04 19:09:55 +0800

    fdsfds

--- backend/Server.js
+++ backend/Server.js
@@ -27,11 +27,7 @@ const allowedOrigins = [
   'http://localhost:3000'
 ];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin: '*',  // <---- TEMP FIX, for debugging only
   credentials: true,
 }));
 
